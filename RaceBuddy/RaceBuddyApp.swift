225cba872ed5e44e87f0bc7d0176461f659718e6
Hisham Bhatti <hishamb@cs.washington.edu>
2024-10-12 13:36:03 -0700

    Initial Commit

---
+++ RaceBuddy/RaceBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaceBuddyApp.swift
+//  RaceBuddy
+//
+//  Created by Hisham Bhatti on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RaceBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RaceBuddyTests/RaceBuddyTests.swift
@@ -0,0 +1,16 @@
+//
+//  RaceBuddyTests.swift
+//  RaceBuddyTests
+//
+//  Created by Hisham Bhatti on 10/12/24.
+//
+
+import Testing
+
+struct RaceBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
